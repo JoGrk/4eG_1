82e4a81c9b6fdcfc1aa280295430a0efadfdbcf7
Jolanta Gryko <jgryko@gmail.com>
2022-11-18 08:48:03 +0100

    portal z błędami

--- egzam/EE.09-05-22.01-SG/uzytkownicy.php
+++ egzam/EE.09-05-22.01-SG/uzytkownicy.php
@@ -14,7 +14,7 @@
     <header id="banerP">
         <?php
         echo "skrypt1";
-        $conn = new mysqli('localhost','root','','4eG_1_portal');
+        $conn = new mysqli('localhost','root','','portal');
         $sql = "select count(*) from dane;";
         $result = $conn -> query($sql);
         $row = $result -> fetch_array();
@@ -46,7 +46,49 @@
         <h3>Wizytówka</h3>
         <div id="wiz">
             <?php
-            echo "skrypt2";
+                $conn = new mysqli('localhost','root','','portal');
+
+                if(isset($_POST['login']) && isset($_POST['haslo'])){
+                    $login = $_POST['login'];
+                    $haslo = sha1($_POST['haslo']);
+
+                    $sql = "select haslo from uzytkownicy 
+                    where login='$login';";
+                    if($result = $conn -> query($sql)){
+                        while($row = $result ->fetch_assoc()){
+                            $row['haslo'];
+                        }
+                        if($row['haslo'] == $haslo){
+                            $sql3 = "select login, rok_urodz,przyjaciol, hobby, zdjecie from uzytkownicy inner join dane on dane.id=uzytkownicy.id where login='$login';";
+                            $res = $conn -> query($sql3);
+                            while($row = $res -> fetch_assoc()){
+                                $login = $row['login'];
+                                $hobby = $row['hobby'];
+                                $przyjaciol = $row['przyjaciol'];
+                                $wiek = DATE('Y') - $row['rok_urodz'];
+                                echo "<img src=".$row['zdjecie']."alt='osoba'> <br>";
+                                echo "<h4> $login ($wiek) <br>";
+                                echo "<p> hobby: $hobby</p>";
+                                echo "<h1><img src='icon-on.png'> $przyjaciol</h1>";
+                                echo "<input type='submit' href='dane.html' value='Więcej informacji'>";
+                            }
+
+                            
+
+
+                        }
+                        else{
+                            echo "Hasło nieprawidłowe";
+                        }
+                    
+                    }
+                    else{
+                        echo "login nie istnieje";
+                    }
+
+                }
+
+                $conn -> close();
             ?>
         </div>
     </section>
