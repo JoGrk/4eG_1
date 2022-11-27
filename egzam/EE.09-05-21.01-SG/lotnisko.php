b629786174cd89610138e60a523a7a95117b3493
Jolanta Gryko <jgryko@gmail.com>
2022-11-27 22:05:12 +0100

    caiasteczka 1

---
+++ egzam/EE.09-05-21.01-SG/lotnisko.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>Port Lotniczy</title>
+	<link rel="stylesheet" href="styl5.css" />
+</head>
+<body>
+	<div id="baner1">
+		<img src="zad5.png" alt="logo lotnisko" />
+	</div>
+	<div id="baner2">
+		<h1>Przyloty</h1>
+	</div>
+	<div id="baner3">
+		<h3>przydatne linki</h3>
+		<a href="kwerendy.txt" target="_blank">Pobierz...</a>
+	</div>
+	<div id="glowny">
+		<table>
+			<tr>
+				<th>czas</th>
+				<th>kierunek</th>
+				<th>numer rejsu</th>
+				<th>status</th>
+			</tr>
+			<?php
+			
+			// skrypt 1
+
+			?>
+		</table>
+	</div>
+	<div id="stopka1">
+	<?php
+
+		// skrypt 2
+
+	?>
+	</div>
+	<div id="stopka2">
+		Autor: PESEL
+	</div>
+</body>
+</html>
